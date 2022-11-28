e78f2cbed247fbcf7a94803c3070e0680defbb8a
enzodevs2000 <enzodevs2000@gmail.com>
2022-11-28 18:25:44 -0300

    feat: Navegacao com o botao sair :sparkles:

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,21 +3,38 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div id='NavBar-container' className="flex bg-azul-escuro w-full justify-between p-2 text-center text-white font-fonte-padrao font-semibold drop-shadow-xl">
+    <div
+      id="NavBar-container"
+      className="flex bg-azul-escuro w-full justify-between p-2 text-center text-white font-fonte-padrao font-semibold drop-shadow-xl"
+    >
       <div id="navbar-logo" className="w-1/3">
         <img src={LogoCJR} alt="logo" />
       </div>
       <div id="navbar-links" className="w-1/3 flex justify-center items-center">
-        <Link to="/membros" className="mr-7 hover:text-btn-login-hover">Planos dos membros</Link>
-        <Link to="/questionarios" className="hover:text-btn-login-hover">Questionários</Link>
+        <Link to="/membros" className="mr-7 hover:text-btn-login-hover">
+          Planos dos membros
+        </Link>
+        <Link to="/questionarios" className="hover:text-btn-login-hover">
+          Questionários
+        </Link>
       </div>
-      <div id="rightCorner-container" className="w-1/3 flex justify-center items-center">
+      <div
+        id="rightCorner-container"
+        className="w-1/3 flex justify-center items-center"
+      >
         <div id="perfil-container" className="flex justify-between mr-5">
-          <div id="username-container" className="mr-3">User</div>
+          <div id="username-container" className="mr-3">
+            User
+          </div>
           <div id="perfil-img-container">perfil img</div>
         </div>
-        <div id="exit-container" className="bg-white text-azul-escuro px-3 py-1 rounded-md hover:bg-btn-login-hover hover:text-white">
-          <button>Sair</button>
+        <div
+          id="exit-container"
+          className="bg-white text-azul-escuro px-3 py-1 rounded-md hover:bg-btn-login-hover hover:text-white"
+        >
+          <Link to="/">
+            <button>Sair</button>
+          </Link>
         </div>
       </div>
     </div>
